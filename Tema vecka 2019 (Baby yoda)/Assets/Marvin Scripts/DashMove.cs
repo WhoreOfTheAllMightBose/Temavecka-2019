90b3f4ac6751e793b4a2cae21d5e620bb9cccabe
SwagoSaurus <47172622+SwagoSaurus@users.noreply.github.com>
2019-12-09 14:14:22 +0100

    Beggining of player yes;) gay
    i made some player stuff yes

---
+++ Tema vecka 2019 (Baby yoda)/Assets/Marvin Scripts/DashMove.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashMove : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float DashSpeed;
+    private float dashTime;
+    public float startDashTime;
+    private int direction;
+
+    public float StartDashTimer;
+    private float DashTimer;
+
+
+
+    void Start()
+    {
+        dashTime = startDashTime;
+    }
+
+    void Update()
+    {
+        if (direction == 0)
+        {
+            if (DashTimer <= 0)
+            {
+                if (Input.GetKeyDown(KeyCode.LeftShift) && Input.GetKey(KeyCode.A))
+                {
+                    direction = 1;
+                    DashTimer = StartDashTimer;
+                }
+                else if (Input.GetKeyDown(KeyCode.LeftShift) && Input.GetKey(KeyCode.D))
+                {
+                    direction = 2;
+                    DashTimer = StartDashTimer;
+                }
+            }
+
+        }
+        else
+        {
+            if (dashTime <= 0)
+            {
+                direction = 0;
+                dashTime = startDashTime;
+                rb.velocity = Vector2.zero;
+                gameObject.layer = 10;
+            }
+            else
+            {
+                dashTime -= Time.deltaTime;
+                gameObject.layer = 12;
+
+                if (direction == 1)
+                {
+                    rb.velocity = Vector2.left * DashSpeed;
+                }
+                else if (direction == 2)
+                {
+                    rb.velocity = Vector2.right * DashSpeed;
+                }
+            }
+        }
+
+        DashTimer -= Time.deltaTime;
+    }
+
+}
---
+++ Tema vecka 2019 (Baby yoda)/Assets/Marvin Scripts/PlayerMovement.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float moveInput;
+    public float Speed;
+    public float JumpForce;
+
+    private Rigidbody rb;
+
+    private bool facingRight = true;
+    private bool sDown = false;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private float jumpTimeCounter;
+    public float JumpTime;
+
+    public bool isJumping;
+
+    private void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = true;
+
+        moveInput = Input.GetAxis("Horizontal") * Speed;
+        moveInput *= Time.fixedDeltaTime;
+        transform.Translate(moveInput, 0, 0);
+
+        if (facingRight == false && moveInput > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            sDown = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            sDown = false;
+        }
+
+        if (!sDown)
+        {
+            if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+            {
+                isJumping = true;
+                jumpTimeCounter = JumpTime;
+                rb.velocity = Vector2.up * JumpForce;
+            }
+
+            if (Input.GetKey(KeyCode.Space) && isJumping == true)
+            {
+                if (jumpTimeCounter > 0)
+                {
+                    rb.velocity = Vector2.up * JumpForce;
+                    jumpTimeCounter -= Time.deltaTime;
+                }
+                else
+                {
+                    isJumping = false;
+                }
+
+            }
+
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                isJumping = false;
+            }
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+}
