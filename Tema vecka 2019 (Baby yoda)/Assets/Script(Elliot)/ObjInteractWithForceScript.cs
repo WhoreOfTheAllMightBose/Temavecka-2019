a5bdbf80d047453a01c607733c44fa0ebe0def2f
ElliotBod <47039662+ElliotBod@users.noreply.github.com>
2019-12-10 12:55:42 +0100

    force stuff

--- Tema vecka 2019 (Baby yoda)/Assets/Script(Elliot)/ObjInteractWithForceScript.cs
+++ Tema vecka 2019 (Baby yoda)/Assets/Script(Elliot)/ObjInteractWithForceScript.cs
@@ -5,24 +5,32 @@ using UnityEngine;
 public class ObjInteractWithForceScript : MonoBehaviour
 {
 
-    public bool floating;
-    bool startSin;
-    float x;
-    float y;
+    public bool Floating;
     public float speed;
+    public bool Pull;
+    public GameObject PointToMoveObjTo;
+    public float HightOfFlot;
+
+    public float TimeStartedLerping;
+    public float LerpTime;
+    bool startedLerp;
+
+    bool startSin;
+    float x, y;
     Vector3 startPos;
 
     private void Start()
     {
-        floating = false;
+        startedLerp = false;
+        Floating = false;
         startPos = transform.position;
     }
 
     private void Update()
     {
-        if(floating)
+        if(Floating)
         {
-            ObjFloating();
+            objFloating();
         }
         else
         {
@@ -31,11 +39,69 @@ public class ObjInteractWithForceScript : MonoBehaviour
             y = 0;
         }
 
+        if(Pull)
+        {
+            
+            startLerping();
+
+            transform.position = lerp(startPos, PointToMoveObjTo.transform.position, TimeStartedLerping, LerpTime);
+
+            if (Vector3.Distance(transform.position, PointToMoveObjTo.transform.position) < 1f)
+            {
+               
+                transform.position = PointToMoveObjTo.transform.position;
+
+                Pull = false;
+                startedLerp = false;
+            }
+        }
     }
 
-    void ObjFloating()
+    public void TriggerObj()
     {
-        x += speed * Time.deltaTime;
+        if(this.tag == "Pull")
+        {
+            Pull = true;
+        }
+        else if (this.tag == "Floating")
+        {
+            Floating = true;
+        }
+        else if (this.tag == "Knock back")
+        {
+
+        }
+    }
+
+    #region pull
+    void startLerping()
+    {
+        if (!startedLerp)
+        {
+            GetComponent<Rigidbody>().useGravity = false;
+            TimeStartedLerping = Time.time;
+        }
+       
+
+        startedLerp = true;
+    }
+
+
+    Vector3 lerp(Vector3 start, Vector3 end, float TimeStartedLerping, float LerpTime = 1)
+   {
+        float timeSinceStarted = Time.time - TimeStartedLerping;
+
+        float procentageComplete = timeSinceStarted / LerpTime;
+
+        return Vector3.Lerp(start, end, procentageComplete);
+   }
+
+    #endregion
+    #region floating obj
+
+    void objFloating()
+    {
+        x += (speed/2) * Time.deltaTime;
 
         if (!startSin)
 
@@ -44,16 +110,12 @@ public class ObjInteractWithForceScript : MonoBehaviour
         if (x > startPos.y + 3)
         {
             startSin = true;
-            y = Mathf.Cos(x);
+            y = HightOfFlot * Mathf.Cos(x);
         }
 
             transform.position = new Vector3(transform.position.x, y);
         
     }
      
-    public void CanFloat()
-    {
-        if (this.tag == "Floating");
-        floating = true;
-    }
+    #endregion
 }
