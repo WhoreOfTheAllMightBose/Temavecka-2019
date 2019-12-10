d3e2f39eb0afb47d5fd5080ae0a7e80e55c0938c
ElliotBod <47039662+ElliotBod@users.noreply.github.com>
2019-12-10 09:10:34 +0100

    flygande box yaa

--- Tema vecka 2019 (Baby yoda)/Assets/Script(Elliot)/ForceScript.cs
+++ Tema vecka 2019 (Baby yoda)/Assets/Script(Elliot)/ForceScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class testScript : MonoBehaviour
+public class ForceScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
---
+++ Tema vecka 2019 (Baby yoda)/Assets/Script(Elliot)/ObjInteractWithForceScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjInteractWithForceScript : MonoBehaviour
+{
+
+    public bool floating;
+    float x;
+    public float speed;
+
+    private void Start()
+    {
+        floating = false;
+    }
+
+    private void Update()
+    {
+        if(floating)
+        {
+            ObjFloating();
+        }
+
+    }
+
+    void ObjFloating()
+    {
+        x += speed * Time.deltaTime;
+
+        if (x != 0)
+            transform.position = new Vector3(transform.position.x, Mathf.Sqrt(Mathf.Sqrt(x)));
+        else
+            x += 1;
+
+        if (transform.position.y >= 1.7f)
+            speed *= -1;
+        if (transform.position.y <= 1)
+            speed *= -1;
+    }
+     
+    public void CanFloat()
+    {
+        if (this.tag == "Floating");
+        floating = true;
+    }
+}
