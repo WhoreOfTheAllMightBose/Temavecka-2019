0c9366c21d294d61feb40d872bd79c3e1bd11a59
Erik <erik.friman@elev.ga.lbs.se>
2019-12-10 14:13:06 +0100

    ABLE TO SPRINT

---
+++ Tema vecka 2019 (Baby yoda)/Assets/Scenes/ErkisSCRIPTS/Animationscript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animationscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Animator>().SetFloat("movement", 1);
+        GetComponent<Animator>().SetTrigger("movement");
+
+
+    }
+}
