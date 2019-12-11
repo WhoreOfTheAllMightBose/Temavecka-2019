b7e41cdb1154e02399cbe9d43edf82a6e449abd9
ElliotBod <47039662+ElliotBod@users.noreply.github.com>
2019-12-11 09:43:11 +0100

    laddade ner snygg button
    kommer mycket skit nu boys

---
+++ Tema vecka 2019 (Baby yoda)/Assets/Elliots stuff/Script(Elliot)/LevelControllerScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelControllerScript : MonoBehaviour
+{
+
+    public static LevelControllerScript instance = null;
+
+
+    int sceneIndex, levelPassed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (instance == null)
+            instance = this;
+        else if (instance != null)
+            Destroy(gameObject);
+
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        levelPassed = PlayerPrefs.GetInt("LevelPassed");
+    }
+
+    public void YouWin()
+    {
+        if (sceneIndex == 3)
+            print("you win");
+        else
+        {
+            if (levelPassed < sceneIndex)
+                PlayerPrefs.SetInt("LevelPassed", sceneIndex);
+            Invoke("loadNextLevel", 1f);
+        }
+    }
+
+    public void GoBack()
+    {
+        Invoke("loadNextLevel", 1f);
+    }
+
+    void loadNextLevel()
+    {
+        SceneManager.LoadScene(sceneIndex + 1);
+
+    }
+
+    void loadLevelMenue()
+    {
+        SceneManager.LoadScene("LevelMenu");
+    }
+
+}
