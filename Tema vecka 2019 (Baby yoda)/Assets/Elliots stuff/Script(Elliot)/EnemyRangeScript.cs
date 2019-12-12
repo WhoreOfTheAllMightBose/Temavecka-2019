8daf0c56c86b989e52e4a51ec17e93bb6402dcd8
ElliotBod <47039662+ElliotBod@users.noreply.github.com>
2019-12-12 13:01:28 +0100

    obj and stuff

---
+++ Tema vecka 2019 (Baby yoda)/Assets/Elliots stuff/Script(Elliot)/EnemyRangeScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRangeScript : EnemyBaseScript
+{
+    public float maxSpeed;
+    bool a;
+    bool playerclose;
+
+    public EnemyRangeScript()
+    {
+        a = false;
+        playerclose = false;
+        DistanceToFollow = 5;
+    }
+
+    public override void Update()
+    {
+        if(ToClose())
+        {
+            reversflip();
+            transform.position += GetDir() * -Speed * Time.deltaTime;
+        }
+        else
+        {
+            reversflip();
+        }
+    }
+
+    void reversflip()
+    {   
+        if(!ToClose() && !playerclose)
+        {
+            Flip();
+            playerclose = true;
+        }
+        else if(ToClose() && playerclose)
+        {
+            Flip();
+            playerclose = false;
+            transform.position += new Vector3(-1.4f, 0, 0);
+        }
+    }
+
+
+}
