eb0b9a32e3cb07c2f8eb4abdca287eb306c9a3d7
ElliotBod <47039662+ElliotBod@users.noreply.github.com>
2019-12-12 10:03:41 +0100

    Enemy stuff

---
+++ Tema vecka 2019 (Baby yoda)/Assets/Elliots stuff/Script(Elliot)/EnemyMaleScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMaleScript : EnemyBaseScript
+{
+    public float maxSpeed;
+    bool a;
+    public EnemyMaleScript()
+    {
+        a = false;
+        DistanceToFollow = 5;
+    }
+
+    public override void Update()
+    {
+        if (a)
+        {
+            Speed += 1 * Time.deltaTime;
+            if (Speed >= maxSpeed)
+                a = false;
+        }
+
+        base.Update();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            // insert that player take dmg
+            print("moist");
+         //   other.gameObject.GetComponent<PlayerHealth>().TakeDamage(3,transform.position);
+            Speed = StartSpeed;
+            a = true;
+        }
+    }
+}
